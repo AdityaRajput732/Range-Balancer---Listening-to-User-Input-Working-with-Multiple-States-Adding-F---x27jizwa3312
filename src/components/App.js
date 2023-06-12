7d46eaa527d540dfa209ed022626b3e6f3e44a0b
ADITYA Raj <108565070+AdityaRajput732@users.noreply.github.com>
2023-06-13 00:04:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,53 +7,60 @@ const App = () => {
   const [valueA, setValueA] = useState(0);
   const [valueB, setValueB] = useState(50);
 
-const handleMaxSumChange = (event) => {
-  const newMaxSum = parseInt(event.target.value);
+  const handleMaxSumChange = (event) => {
+    const newMaxSum = parseInt(event.target.value);
 
-  if (!isNaN(newMaxSum)) {
-    const newRangeA = Math.min(rangeA, newMaxSum - valueB);
-    const newRangeB = Math.min(rangeB, newMaxSum - valueA);
-
-    setMaxSum(newMaxSum);
-    setRangeA(newRangeA);
-    setRangeB(newRangeB);
-  }
-};
-
-
-
+    if (!isNaN(newMaxSum)) {
+      setMaxSum(newMaxSum);
+      setRangeA(Math.min(rangeA, newMaxSum - valueB));
+      setRangeB(Math.min(rangeB, newMaxSum - valueA));
+    }
+  };
 
   const handleRangeAChange = (event) => {
-    // setRangeA(parseInt(maxSum)-parseInt(rangeB));
-    setRangeB(parseInt(maxSum)-parseInt(event.target.value));
-    setValueA(parseInt(event.target.value))
-    // setMaxSum(parseInt(rangeB) - parseInt(event.target.value));
+    const newValueA = parseInt(event.target.value);
+
+    if (!isNaN(newValueA)) {
+      setValueA(newValueA);
+    }
   };
 
   const handleRangeBChange = (event) => {
-    console.log(event.target.value);
-    setRangeB(parseInt(maxSum)-parseInt(valueA));
-    setRangeA(parseInt(maxSum)-parseInt(event.target.value))
-    setValueB(event.target.value)
-    // setMaxSum(parseInt(valueA) + parseInt(event.target.value));
+    const newValueB = parseInt(event.target.value);
+
+    if (!isNaN(newValueB)) {
+      setValueB(newValueB);
+    }
   };
-  // console.log(rangeA, rangeB, maxSum);
 
   return (
     <div>
       <div id="max-sum-holder">
         Input max sum:-
-        <input type="number" value={maxSum} onChange={handleMaxSumChange} />
+        <input
+          type="number"
+          value={maxSum}
+          onChange={handleMaxSumChange}
+        />
       </div>
-    <div id="range-a-holder">
-  <input type="range" value={valueA} max={rangeA} onChange={handleRangeAChange} />
-  <span id="range-a-value">{valueA}</span>
-</div>
-<div id="range-b-holder">
-  <input type="range" value={valueB} max={rangeB} onChange={handleRangeBChange} />
-  <span id="range-b-value">{valueB}</span>
-</div>
-
+      <div id="range-a-holder">
+        <input
+          type="range"
+          value={valueA}
+          max={rangeA}
+          onChange={handleRangeAChange}
+        />
+        <span id="range-a-value">{valueA}</span>
+      </div>
+      <div id="range-b-holder">
+        <input
+          type="range"
+          value={valueB}
+          max={rangeB}
+          onChange={handleRangeBChange}
+        />
+        <span id="range-b-value">{valueB}</span>
+      </div>
       <div id="sum">{parseInt(valueA) + parseInt(valueB)}</div>
     </div>
   );
